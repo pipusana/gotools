fc064f0c21e00a47233f467630cff73dcd2d8bb8
pipusana <>
2018-07-21 16:52:17 +0700

    fetch api git hub

--- src/github.com/pipusana/hello-go/hello.go
+++ src/github.com/pipusana/hello-go/hello.go
@@ -2,15 +2,68 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/pipusana/gotools"
 )
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func (p Person) fullname() string {
+	return fmt.Sprintf("%s %s", p.FirstName, p.LastName)
+}
+
+func (p *Person) growup() {
+	p.Age = p.Age + 1
+}
 
 func init() {
 	fmt.Println("init")
 }
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println(gotools.Add(10, 2))
+	name := map[string]int{
+		"a": 20,
+		"b": 30,
+		"c": 40,
+	}
+
+	for k, v := range name {
+		fmt.Println(k, v)
+	}
+
+	// me := Person{
+	// 	FirstName: "Pipusana",
+	// 	LastName:  "petgumpoom",
+	// 	Age:       24,
+	// }
+
+	// fmt.Printf("%+v", me)
+
+	people := []Person{
+		Person{
+			FirstName: "Pipusana",
+			LastName:  "petgumpoom",
+			Age:       24,
+		},
+		Person{
+			FirstName: "ter",
+			LastName:  "bone",
+			Age:       23,
+		},
+	}
+
+	for _, p := range people {
+		fmt.Println(p.fullname())
+		p.growup()
+
+		fmt.Sprintf("%d", p.Age)
+
+		// result, err := json.Marshal(p)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Println(string(fullname))
+	}
 }
